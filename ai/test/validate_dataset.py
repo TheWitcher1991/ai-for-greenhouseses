a5d196ce20062c8f85ff5828aab0e256f7bd4585
TheWitcher1991 <ashot.svazyan@yandex.ru>
2025-12-27 11:59:25 +0300

    feat(core): add loop adapter

--- ai/test/validate_dataset.py
+++ ai/test/validate_dataset.py
@@ -1,8 +1,8 @@
 from framework.transforms import ComposeTransforms
-from sdk.v2.dataset.coco import CocoSegmentationDataset
+from framework.detection.v1.dataset.coco import CocoSegmentationDataset
 
 ds = CocoSegmentationDataset(
-    images_dir="data/v2/images", annotation_file="data/v2/annotations.json", transforms=ComposeTransforms()
+    images_dir="data/v1/images", annotation_file="data/v1/annotations.json", transforms=ComposeTransforms()
 )
 
 for i in range(len(ds)):
